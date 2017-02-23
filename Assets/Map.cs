212d9525d71e76e483542000629bf576851cbe35
Brian Palmer <brian@codekitchen.net>
2017-02-23 16:32:02 -0700

    initial flat plane impl

---
+++ Assets/Map.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour {
+  public float size = 20;
+  public int pixels = 20;
+
+  public float moveSpeed = 6f;
+  public Vector2 position = Vector2.zero;
+
+  public WaveSystem waves;
+
+  public Transform pixelPrefab;
+
+  Transform gridObject;
+  Transform[,] grid;
+
+  /// <summary>
+  /// Start is called on the frame when a script is enabled just before
+  /// any of the Update methods is called the first time.
+  /// </summary>
+  void Start() {
+    CreateGrid();
+    SetupCamera();
+  }
+
+  /// <summary>
+  /// Update is called every frame, if the MonoBehaviour is enabled.
+  /// </summary>
+  void Update() {
+    position.x += Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+    position.y += Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
+
+    Vector3 offset = new Vector3(position.x - Mathf.Floor(position.x), 0, position.y - Mathf.Floor(position.y)) * -stride;
+    for (int y = 0; y < pixels; ++y) {
+      for (int x = 0; x < pixels; ++x) {
+        var pixel = grid[y, x];
+        var waveMotion = waves.WaveOffset(gridToWorld(new Vector2(x, y)));
+        waveMotion = Vector3.zero;
+        pixel.position = BasePos(x, y) + offset + waveMotion;
+      }
+    }
+  }
+
+  Vector2 gridToWorld(Vector2 gridPos) {
+    return new Vector2(
+      gridPos.x + Mathf.Floor(position.x),
+      gridPos.y + Mathf.Floor(position.y)
+    );
+  }
+
+  float Perlin(float x, float y) {
+    return Mathf.PerlinNoise(x * 0.05f, y * 0.005f);
+  }
+
+  float stride { get { return size / (float)(pixels - 1); } }
+
+  Vector3 BasePos(int x, int y) {
+    float yp = -(size / 2) + y * stride;
+    float xp = -(size / 2) + x * stride;
+    return new Vector3(xp, 0, yp);
+  }
+
+  void CreateGrid() {
+    if (gridObject)
+      Destroy(gridObject.gameObject);
+    gridObject = new GameObject("Grid").transform;
+    gridObject.SetParent(transform);
+
+    grid = new Transform[pixels, pixels];
+
+    for (int y = 0; y < pixels; ++y) {
+      for (int x = 0; x < pixels; ++x) {
+        grid[y, x] = Instantiate(pixelPrefab, BasePos(x, y), Quaternion.identity, gridObject);
+      }
+    }
+  }
+
+  void SetupCamera() {
+    var cam = Camera.main.transform;
+    cam.position = new Vector3(0, size, 0);
+    cam.LookAt(Vector3.zero);
+  }
+}
---
+++ Assets/WaveSystem.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class WaveSystem {
+  [System.Serializable]
+  public struct Wave {
+    public float wavelength;
+    public float amplitude;
+    public float speed;
+    [Range(0f, 360f)]
+    public float angle;
+    [Range(0f, 1f)]
+    public float steepness;
+    public Vector2 direction { get { return new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad)); } }
+  }
+  public List<Wave> waves = new List<Wave>();
+
+  const float HalfPI = Mathf.PI * 0.5f;
+  const float TwoPI = Mathf.PI * 2f;
+
+  public Vector3 WaveOffset(Vector2 position) {
+    Vector3 offset = Vector3.zero;
+    foreach (Wave wave in waves) {
+      float freq = TwoPI / wave.wavelength;
+      float phase = wave.speed * freq;
+      float steepness = wave.steepness / (freq * wave.amplitude * waves.Count);
+      Vector2 direction = wave.direction;
+      offset.x += steepness * wave.amplitude * direction.x * Mathf.Cos(Vector2.Dot(position, freq * direction) + phase * Time.time);
+      offset.z += steepness * wave.amplitude * direction.y * Mathf.Cos(Vector2.Dot(position, freq * direction) + phase * Time.time);
+      offset.y += wave.amplitude * Mathf.Sin(Vector2.Dot(position, freq * direction) + phase * Time.time);
+    }
+    return offset;
+  }
+}
